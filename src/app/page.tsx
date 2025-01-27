4726d0bbeda66c35d747b1a71e656fb0c4bb9f08
RuanDevJs <ruanelpidio@hotmail.com>
2025-01-27 11:22:09 -0300

    Finished Project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,64 @@
-import Image from "next/image";
+'use client'
+
+import { Eye, EyeSlash } from 'phosphor-react'
+
+import VertigoLogo from "@/assets/Vertigo_Logo.svg"
+import Image from 'next/image'
+import { useState } from 'react'
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [inputType, setInputType] = useState<'password' | 'text'>('password');
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const handleInputType = () => {
+    return setInputType(oldValue => oldValue === 'password' ? 'text' : 'password')
+  }
+
+  return (
+    <main className='grid grid-cols-2 h-dvh bg-[#F8FAFC]'>
+      <div className='p-10'>
+        <div>
+          <Image src={VertigoLogo} alt="" />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        <form className='mt-14 min-w-[360px] max-w-[480px]'>
+          <h1 className='font-bold text-4xl text-[#1E293B]'>Acesse a plataforma</h1>
+          <p className='font-normal text-base text-[#475569] py-3'>
+            Faça login ou registre-se para começar a construir <br /> seus projetos ainda hoje.
+          </p>
+
+          <div className='mb-3'>
+            <label className='block font-semibold text-sm py-2'>E-mail</label>
+            <input type="email" placeholder='Digite seu e-mail' className='w-full p-3 outline-none bg-[#fff] border border-[#E2E8F0] rounded' />
+          </div>
+
+          <div className='mb-3'>
+            <label className='flex justify-between font-semibold text-sm py-2'>
+              <span>Senha</span>
+              <a href="#" className='text-[#7C3AED] hover:brightness-125 transition ease-in-out duration-150'>Esqueceu a senha ?</a>
+            </label>
+            <div className='w-full p-3 outline-none bg-[#fff] border border-[#E2E8F0] rounded flex items-center justify-between'>
+              <input
+                type={inputType}
+                placeholder='Digite sua senha'
+                className='outline-none border-none w-full'
+              />
+              <div className='cursor-pointer' onClick={handleInputType}>
+                {inputType === 'password' && <EyeSlash size={25} color="#94A3B8" />}
+                {inputType === 'text' && <Eye size={25} color="#94A3B8" />}
+              </div>
+            </div>
+          </div>
+
+          <div className='mt-5'>
+            <button className='w-full bg-[#7C3AED] p-3.5 rounded font-bold text-white font-lg hover:brightness-125 transition ease-in-out duration-150'>
+              Entrar
+            </button>
+            <p className='font-normal text-zinc-500 block mt-3'>Ainda não tem uma conta ?
+              <a href="#" className='text-[#7C3AED] font-bold ml-1 hover:brightness-125 transition ease-in-out duration-150'>Inscreva-se</a>
+            </p>
+          </div>
+        </form>
+      </div>
+      <div className='background-image' />
+    </main>
+  )
 }
